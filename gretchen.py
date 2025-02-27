fde63ccd6dbe33e5c326b7aae477b5abb6a12a8d
ekrohne <krohneew@gmail.com>
2025-02-26 17:38:42 -0800

    Hannah with reschedule button, including "now".

--- gretchen.py
+++ gretchen.py
@@ -1,87 +1,222 @@
-# This example requires the 'message_content' privileged intent to function.
-
-if __file__.endswith('gretchen.py'):
-    import gretchen_credentials as credentials
-elif __file__.endswith('hannah.py'):
-    import hannah_credentials as credentials
-else:
-    print("unrecognized file ending")
-
-from discord.ext import commands
-
+import datetime
+import math
+import random
+import asyncio
+import logging
+import importlib
+import dataclasses
 import discord
+from discord import app_commands
 
-class Bot(commands.Bot):
-    def __init__(self):
-        intents = discord.Intents.default()
-        intents.message_content = True
+second = datetime.timedelta(seconds=1)
+minute = datetime.timedelta(minutes=1)
+hour = datetime.timedelta(hours=1)
+day = datetime.timedelta(days=1)
 
-        super().__init__(command_prefix=commands.when_mentioned_or('$'), intents=intents)
+def as_string(dt):
+    return dt.strftime("**%H:%M**")
+
+for name in "gretchen", "hannah":
+    if __file__.endswith(f"{name}.py"):
+        credentials = importlib.import_module(f"{name}_credentials")
+        print(name)
+        logging.basicConfig(filename=f"{name}.log", filemode="w", level=logging.INFO)
+        logger = logging.getLogger(name)
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        #commands = await self.tree.sync()
+        pass
+
+@dataclasses.dataclass
+class RescheduleMessage:
+    earliest: datetime.datetime
+    latest: datetime.datetime
+    start_time: datetime.datetime
+    interaction: discord.Interaction
 
 
+intents = discord.Intents.default()
+# intents.members = True
+client = MyClient(intents=intents)
 
-# Define a simple View that gives us a confirmation menu
-class Confirm(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.value = None
+me = None
+drk = None
+queue = asyncio.Queue()
+start_time = None
 
-    # When the confirm button is pressed, set the inner value to `True` and
-    # stop the View from listening to more input.
-    # We also send the user an ephemeral message that we're confirming their choice.
-    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
-    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Confirming', ephemeral=True)
-        self.value = True
-        # self.stop()
-
-    # This one is similar to the confirmation button except sets the inner value to `False`
-    @discord.ui.button(label='Cancel', style=discord.ButtonStyle.grey)
-    async def cancel(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_message('Cancelling', ephemeral=True)
-        self.value = False
-        # self.stop()
+class RescheduleException(Exception):
+    def __init__(self, message: RescheduleMessage):
+        for field in dataclasses.fields(message):
+            setattr(self, field.name, getattr(message, field.name))
 
 
-bot = Bot()
-me, drk = None, None
-@bot.event
+async def at(timeout):
+    global queue
+
+    now = datetime.datetime.now()
+    try:
+        message = await asyncio.wait_for(queue.get(), (timeout-now).total_seconds())
+    except asyncio.TimeoutError:
+        return
+    else:
+        raise RescheduleException(message)
+
+async def at_noreschedule(timeout):
+    while True:
+        try:
+            await at(timeout)
+            break
+        except RescheduleException as ex:
+            logger.debug(f"Session already begun. Cannot reschdule.")
+            await ex.interaction.response.send_message(f"Ed's edging has already begun. Cannot reschedule.")
+            continue
+
+def random_minute_between(earliest: datetime.datetime, latest: datetime.datetime):
+    seconds_between = int((latest-earliest).total_seconds())
+    if seconds_between < 60:
+        return earliest
+
+    while True:
+        random_time = earliest + random.randint(0, int((latest-earliest).total_seconds()))*second
+
+        random_minute = datetime.datetime(random_time.year, random_time.month, random_time.day, random_time.hour, random_time.minute)
+        if random_minute < earliest or random_minute > latest:
+            continue
+
+        return random_minute
+
+@client.event
 async def on_ready():
-    global bot, me, drk
-
-    me = await bot.fetch_user(credentials.peeps['drk'])
-    drk = await bot.fetch_user(credentials.peeps['drk'])
-
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    global client, me, drk, queue, start_time
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
     print('------')
 
-    view = Confirm()
-    await drk.send('Do you want to continue?', view=view)
-    # Wait for the View to stop listening for input...
-    await view.wait()
+    try:
+        me = await client.fetch_user(credentials.peeps['me'])
+        drk = await client.fetch_user(credentials.peeps['drk'])
 
-    await drk.send('View is now finished.')
-    if view.value is None:
-        await drk.send('Timed out.')
-    elif view.value:
-        await drk.send('Confirmed.')
+        now = datetime.datetime.now()
+        today = datetime.datetime(now.year, now.month, now.day)
+        start_time = math.ceil((now - today).total_seconds()/60)*60*second + today
+
+        logger.info(f'Beginning daily run')
+        now = datetime.datetime.now()
+        midnight = datetime.datetime(now.year, now.month, now.day) + 1*day
+        noon = datetime.datetime(now.year, now.month, now.day) + 12*hour
+        start_time = random_minute_between(max(now, noon)+30*minute, midnight)
+        day_of_week = now.strftime("%A")
+
+        await drk.send(f"Notifying Master Emily of today's edging start time.")
+        await me.send(
+            f"Master Emily, today, {day_of_week}, Ed will be required to begin edging at {as_string(start_time)}."
+            f"\n\nEd will not know when he will be edging today until his thirty minute advance notice at {as_string(start_time-30*minute)}. He will also be given five minute and one minute warnings."
+        )
+
+        while True:
+            try:
+                await at(start_time-30*minute)
+                logger.info(f'Sending thirty minute warning to Dr. Krohne')
+                await drk.send(
+                    f"This is your notification that you will be edging in 30 minutes. You must begin at {as_string(start_time)}.")
+
+            except RescheduleException as ex:
+                logger.debug(f"Requested {as_string(ex.earliest)} to {as_string(ex.latest)}")
+                await ex.interaction.response.send_message(f"Requested {as_string(ex.earliest)} to {as_string(ex.latest)}")
+                start_time = ex.start_time
+
+                await me.send(f"The new start time will be {as_string(start_time)} with an advance notice at {as_string(start_time - 30*minute)}.")
+                logger.debug(f"The new start time will be {as_string(start_time)} with an advance notice at {as_string(start_time - 30*minute)}.")
+                continue
+
+            try:
+                await at(start_time - 10*minute)
+                logger.info(f'Sending ten minute warning to Master Emily')
+                await me.send(f"This is your ten minute reminder that Ed will start edging. He will start at {as_string(start_time)}.")
+
+                await at(start_time - 5*minute)
+                logger.info(f'Sending five minute warning to Dr. Krohne')
+                await drk.send(f"This is your five minute warning to begin edging. You must begin at {as_string(start_time)}.")
+
+                await at(start_time - 1*minute)
+                logger.info(f'Sending one minute warning to Dr. Krohne')
+                await drk.send(f"This is your one minute warning to begin edging. You must begin at {as_string(start_time)}.")
+
+                await at(start_time)
+                logger.info(f'Sending timeout to Dr. Krohne')
+                await drk.send(f"You must now be actively edging.")
+
+            except RescheduleException as ex:
+                logger.debug(f"Requested {as_string(ex.earliest)} to {as_string(ex.latest)}")
+                await ex.interaction.response.send_message(f"Requested {as_string(ex.earliest)} to {as_string(ex.latest)}")
+                start_time = ex.start_time
+
+                await drk.send(f"Master Emily has manually rescheduled today's edging start time. It will no longer be at {as_string(start_time)}.")
+                await me.send(f"Because Ed had already been notified of today's edging start time of {as_string(start_time)}, he has been told that you requested a manual reschedule, though not when.")
+
+                await me.send(f"The new start time will be {as_string(start_time)} with an advance notice at {as_string(start_time - 30*minute)}.")
+                logger.debug(f"The new start time will be {as_string(start_time)} with an advance notice at {as_string(start_time - 30*minute)}.")
+                continue
+
+            for s in range(0,90,15):
+                await at_noreschedule(start_time + s*second)
+                await drk.send(f"You have {90-s} seconds to be close to orgasm.")
+
+            await at_noreschedule(start_time + 90*second)
+            await drk.send(f"You must be close to orgasm.")
+
+            await at_noreschedule(start_time + 10*minute)
+            await drk.send(f"You may stop or continue.")
+
+            await at_noreschedule(start_time + 30*minute)
+            await drk.send(f"You must stop if you have not.")
+
+            break
+    finally:
+        await client.close()
+
+@client.tree.command()
+@app_commands.choices(choice=[
+    app_commands.Choice(name="immediately", value=-1),
+    app_commands.Choice(name="in 30 minutes", value=0),
+    *(
+        app_commands.Choice(name=f"{n}:00 to {n+1}:59", value=n)
+        for n in range(12, 24, 2)
+    )
+])
+async def reschedule(interaction: discord.Interaction, choice: app_commands.Choice[int]):
+    if interaction.user != me:
+        await interaction.response.send_message(f"Only Master Emily can reschedule.")
+        await me.send(f"{interaction.user} requested a manual reschedule, which is not allowed.")
+        return
+
+    if choice.value == 0:
+        now = datetime.datetime.now()
+        today = datetime.datetime(now.year, now.month, now.day)
+        earliest = today + 30*minute + math.ceil((now-today).total_seconds()/60)*minute
+        latest = earliest
     else:
-        await drk.send('Canceled.')
+        now = datetime.datetime.now()
+        today = datetime.datetime(now.year, now.month, now.day)
+        earliest = today + choice.value*hour
+        latest = earliest + 119*minute
 
-@bot.command()
-async def ask(ctx: commands.Context):
-    """Asks the user a question to confirm something."""
-    # We create the view and assign it to a variable so we can wait for it later.
-    view = Confirm()
-    await ctx.send('Do you want to continue?', view=view)
-    # Wait for the View to stop listening for input...
-    await view.wait()
-    if view.value is None:
-        print('Timed out...')
-    elif view.value:
-        print('Confirmed...')
-    else:
-        print('Cancelled...')
+    earliest = max(earliest, now+30*minute)
+    if earliest > latest:
+        await interaction.response.send_message(f"Impossible reschedule: this time slot will be over before Ed can be given a 30 minute notification.")
+        return
 
+    queue.put_nowait(
+        RescheduleMessage(
+            earliest=earliest,
+            latest=latest,
+            start_time=random_minute_between(earliest, latest),
+            interaction=interaction
+        )
+    )
 
-bot.run(credentials.bot_token)
+client.run(credentials.bot_token)
--- hannah.py
+++ hannah.py
@@ -1,161 +1,221 @@
-from typing import Optional
 import datetime
+import math
 import random
 import asyncio
 import logging
-
+import importlib
+import dataclasses
 import discord
 from discord import app_commands
-import hannah_credentials
 
-logging.basicConfig(filename="hannah.log", level=logging.INFO)
-logger = logging.getLogger('hannah')
-
-MY_GUILD = discord.Object(id=hannah_credentials.peeps['greystone'])  # replace with your guild id
-
-class MyClient(discord.Client):
-    def __init__(self, *, intents: discord.Intents):
-        super().__init__(intents=intents)
-        # A CommandTree is a special type that holds all the application command
-        # state required to make it work. This is a separate class because it
-        # allows all the extra state to be opt-in.
-        # Whenever you want to work with application commands, your tree is used
-        # to store and work with them.
-        # Note: When using commands.Bot instead of discord.Client, the bot will
-        # maintain its own tree instead.
-        self.tree = app_commands.CommandTree(self)
-
-    # In this basic example, we just synchronize the app commands to one guild.
-    # Instead of specifying a guild to every command, we copy over our global commands instead.
-    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
-    async def setup_hook(self):
-        # This copies the global commands over to your guild.
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-client = MyClient(intents=intents)
-
-@client.tree.command()
-async def hello(interaction: discord.Interaction):
-    global client, me, drk
-
-    # We create the view and assign it to a variable so we can wait for it later.
-    # me = await client.fetch_user(hannah_credentials.peeps['drk'])
-    # drk = await client.fetch_user(hannah_credentials.peeps['drk'])
-    channel = interaction.channel
-    view = Engage("Button from command text")
-    await interaction.response.send_message("Asking Master Emily", ephemeral=True)
-    await me.send('Master Emily, would you please hit "confirm" to confirm to Ed that he can write scripts that send messages asking questions?', view=view)
-
-    # Wait for the View to stop listening for input...
-    await view.wait()
-    if view.value is None:
-        print('Timed out...')
-    elif view.value:
-        await drk.send("Master Emily Confirmed")
-        print('Confirmed...')
-    else:
-        await drk.send("Master Emily Cancelled")
-        print('Cancelled...')
-
-class Engage(discord.ui.View):
-    def __init__(self, button_text: str):
-        super().__init__()
-        self.button_text = button_text
-        self.value = None
-        self.confirm.label = button_text
-
-    # When the confirm button is pressed, set the inner value to `True` and
-    # stop the View from listening to more input.
-    # We also send the user an ephemeral message that we're confirming their choice.
-    @discord.ui.button(label='Confirm', style=discord.ButtonStyle.green)
-    async def confirm(self, interaction: discord.Interaction, button: discord.ui.Button):
-        self.value = True
-
-me = None
-drk = None
-
-@client.event
-async def on_ready():
-    global client, me, drk
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
-    print('------')
-
-    me = await client.fetch_user(hannah_credentials.peeps['me'])
-    drk = await client.fetch_user(hannah_credentials.peeps['drk'])
-
-    try:
-        logger.info(f'Beginning daily run')
-        intents = discord.Intents.default()
-        intents.members = True
-        client = discord.Client(intents=intents)
-
-        now = datetime.datetime.now()
-        end_of_day = datetime.datetime(now.year, now.month, now.day) + 1*day
-        seconds_to_wait = random.randint(0, (end_of_day - now).seconds)
-        logger.debug(f"Will send message in {seconds_to_wait} seconds")
-
-        drk_notification_time = as_string(now + seconds_to_wait*second)
-        drk_start_time = as_string(now + seconds_to_wait*second + 30*minute)
-        day_of_week = now.strftime("%A")
-
-        view = Engage("Button text")
-        await drk.send("Notifying Master Emily of today's edging start time.", view=view)
-        await me.send(
-            f"Master Emily, today, {day_of_week}, Ed will be required to begin edging at {drk_start_time}."
-            f"\n\nEd will not know when he will be edging today until his thirty minute advance notice at {drk_notification_time}. He will also be given a five minute and one minute warnings."
-        )
-        await sleep_timedelta(seconds_to_wait*second)
-
-        logger.info(f'Sending five minute warning to Dr. Krohne')
-        await drk.send(f"This is your notification that you will be edging in 30 minutes. You must begin at {drk_start_time}.")
-
-        await sleep_timedelta(20*minute)
-        logger.info(f'Sending ten minute warning to Master Emily')
-        await me.send(f"This is your ten minute reminder that Ed will start edging. He will start at {drk_start_time}.")
-
-        await sleep_timedelta(5*minute)
-        logger.info(f'Sending message to Dr. Krohne')
-        await drk.send(f"This is your five minute warning to begin edging. You must begin at {drk_start_time}.")
-
-        await sleep_timedelta(4*minute)
-        logger.info(f'Sending message to Dr. Krohne')
-        await drk.send(f"This is your one minute warning to begin edging. You must begin at {drk_start_time}.")
-
-        await sleep_timedelta(1*minute)
-        await drk.send(f"You must now be actively edging.")
-
-        for t in range(90,0,-15):
-            await drk.send(f"You have {t} seconds to be close to orgasm.")
-            await sleep_timedelta(15*second)
-
-        await drk.send(f"You must be close to orgasm.")
-        await sleep_timedelta(10*minute-90*second)
-        await drk.send(f"You may stop, or continue.")
-        await sleep_timedelta(20*minute)
-        await drk.send(f"You must stop, if you have not.")
-
-    except Exception as e:
-        logger.exception(e)
-        raise
-    finally:
-        await client.close()
+second = datetime.timedelta(seconds=1)
+minute = datetime.timedelta(minutes=1)
+hour = datetime.timedelta(hours=1)
+day = datetime.timedelta(days=1)
 
 def as_string(dt):
     return dt.strftime("**%H:%M**")
 
-second = datetime.timedelta(seconds=1)
-minute = datetime.timedelta(minutes=1)
-hour=datetime.timedelta(hours=1)
-day=datetime.timedelta(days=1)
+for name in "gretchen", "hannah":
+    if __file__.endswith(f"{name}.py"):
+        credentials = importlib.import_module(f"{name}_credentials")
+        print(name)
+        logging.basicConfig(filename=f"{name}.log", filemode="w", level=logging.INFO)
+        logger = logging.getLogger(name)
 
-async def sleep_timedelta(td):
-    await asyncio.sleep(td.seconds)
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
 
-client.run(hannah_credentials.bot_token)
+    async def setup_hook(self):
+        #commands = await self.tree.sync()
+        pass
 
-import sys
-sys.exit()
+@dataclasses.dataclass
+class RescheduleMessage:
+    earliest: datetime.datetime
+    latest: datetime.datetime
+    start_time: datetime.datetime
+    interaction: discord.Interaction
+
+
+intents = discord.Intents.default()
+# intents.members = True
+client = MyClient(intents=intents)
+
+me = None
+drk = None
+queue = asyncio.Queue()
+start_time = None
+
+class RescheduleException(Exception):
+    def __init__(self, message: RescheduleMessage):
+        for field in dataclasses.fields(message):
+            setattr(self, field.name, getattr(message, field.name))
+
+
+async def at(timeout):
+    global queue
+
+    now = datetime.datetime.now()
+    try:
+        message = await asyncio.wait_for(queue.get(), (timeout-now).total_seconds())
+    except asyncio.TimeoutError:
+        return
+    else:
+        raise RescheduleException(message)
+
+async def at_noreschedule(timeout):
+    while True:
+        try:
+            await at(timeout)
+            break
+        except RescheduleException as ex:
+            logger.debug(f"Session already begun. Cannot reschdule.")
+            await ex.interaction.response.send_message(f"Ed's edging has already begun. Cannot reschedule.")
+            continue
+
+def random_minute_between(earliest: datetime.datetime, latest: datetime.datetime):
+    seconds_between = int((latest-earliest).total_seconds())
+    if seconds_between < 60:
+        return earliest
+
+    while True:
+        random_time = earliest + random.randint(0, int((latest-earliest).total_seconds()))*second
+
+        random_minute = datetime.datetime(random_time.year, random_time.month, random_time.day, random_time.hour, random_time.minute)
+        if random_minute < earliest or random_minute > latest:
+            continue
+
+        return random_minute
+
+@client.event
+async def on_ready():
+    global client, me, drk, queue, start_time
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+    try:
+        me = await client.fetch_user(credentials.peeps['me'])
+        drk = await client.fetch_user(credentials.peeps['drk'])
+
+        now = datetime.datetime.now()
+        today = datetime.datetime(now.year, now.month, now.day)
+        start_time = math.ceil((now - today).total_seconds()/60)*60*second + today
+
+        logger.info(f'Beginning daily run')
+        now = datetime.datetime.now()
+        midnight = datetime.datetime(now.year, now.month, now.day) + 1*day
+        noon = datetime.datetime(now.year, now.month, now.day) + 12*hour
+        start_time = random_minute_between(max(now, noon)+30*minute, midnight)
+        day_of_week = now.strftime("%A")
+
+        await drk.send(f"Notifying Master Emily of today's edging start time.")
+        await me.send(
+            f"Master Emily, today, {day_of_week}, Ed will be required to begin edging at {as_string(start_time)}."
+            f"\n\nEd will not know when he will be edging today until his thirty minute advance notice at {as_string(start_time-30*minute)}. He will also be given five minute and one minute warnings."
+        )
+
+        while True:
+            try:
+                await at(start_time-30*minute)
+                logger.info(f'Sending thirty minute warning to Dr. Krohne')
+                await drk.send(
+                    f"This is your notification that you will be edging in 30 minutes. You must begin at {as_string(start_time)}.")
+
+            except RescheduleException as ex:
+                logger.debug(f"Requested {as_string(ex.earliest)} to {as_string(ex.latest)}")
+                await ex.interaction.response.send_message(f"Requested {as_string(ex.earliest)} to {as_string(ex.latest)}")
+                start_time = ex.start_time
+
+                await me.send(f"The new start time will be {as_string(start_time)} with an advance notice at {as_string(start_time - 30*minute)}.")
+                logger.debug(f"The new start time will be {as_string(start_time)} with an advance notice at {as_string(start_time - 30*minute)}.")
+                continue
+
+            try:
+                await at(start_time - 10*minute)
+                logger.info(f'Sending ten minute warning to Master Emily')
+                await me.send(f"This is your ten minute reminder that Ed will start edging. He will start at {as_string(start_time)}.")
+
+                await at(start_time - 5*minute)
+                logger.info(f'Sending five minute warning to Dr. Krohne')
+                await drk.send(f"This is your five minute warning to begin edging. You must begin at {as_string(start_time)}.")
+
+                await at(start_time - 1*minute)
+                logger.info(f'Sending one minute warning to Dr. Krohne')
+                await drk.send(f"This is your one minute warning to begin edging. You must begin at {as_string(start_time)}.")
+
+                await at(start_time)
+                logger.info(f'Sending timeout to Dr. Krohne')
+                await drk.send(f"You must now be actively edging.")
+
+            except RescheduleException as ex:
+                logger.debug(f"Requested {as_string(ex.earliest)} to {as_string(ex.latest)}")
+                await ex.interaction.response.send_message(f"Requested {as_string(ex.earliest)} to {as_string(ex.latest)}")
+                start_time = ex.start_time
+
+                await drk.send(f"Master Emily has manually rescheduled today's edging start time. It will no longer be at {as_string(start_time)}.")
+                await me.send(f"Because Ed had already been notified of today's edging start time of {as_string(start_time)}, he has been told that you requested a manual reschedule, though not when.")
+
+                await me.send(f"The new start time will be {as_string(start_time)} with an advance notice at {as_string(start_time - 30*minute)}.")
+                logger.debug(f"The new start time will be {as_string(start_time)} with an advance notice at {as_string(start_time - 30*minute)}.")
+                continue
+
+            for s in range(0,90,15):
+                await at_noreschedule(start_time + s*second)
+                await drk.send(f"You have {90-s} seconds to be close to orgasm.")
+
+            await at_noreschedule(start_time + 90*second)
+            await drk.send(f"You must be close to orgasm.")
+
+            await at_noreschedule(start_time + 10*minute)
+            await drk.send(f"You may stop or continue.")
+
+            await at_noreschedule(start_time + 30*minute)
+            await drk.send(f"You must stop if you have not.")
+
+            break
+    finally:
+        await client.close()
+
+@client.tree.command()
+@app_commands.choices(choice=[
+    app_commands.Choice(name="now", value=0),
+    *(
+        app_commands.Choice(name=f"{n}:00 to {n+1}:59", value=n)
+        for n in range(12, 24, 2)
+    )
+])
+async def reschedule(interaction: discord.Interaction, choice: app_commands.Choice[int]):
+    if interaction.user != me:
+        await interaction.response.send_message(f"Only Master Emily can reschedule.")
+        await me.send(f"{interaction.user} requested a manual reschedule, which is not allowed.")
+        return
+
+    if choice.value == 0:
+        now = datetime.datetime.now()
+        today = datetime.datetime(now.year, now.month, now.day)
+        earliest = today + 30*minute + math.ceil((now-today).total_seconds()/60)*minute
+        latest = earliest
+    else:
+        now = datetime.datetime.now()
+        today = datetime.datetime(now.year, now.month, now.day)
+        earliest = today + choice.value*hour
+        latest = earliest + 119*minute
+
+    earliest = max(earliest, now+30*minute)
+    if earliest > latest:
+        await interaction.response.send_message(f"Impossible reschedule: this time slot will be over before Ed can be given a 30 minute notification.")
+        return
+
+    queue.put_nowait(
+        RescheduleMessage(
+            earliest=earliest,
+            latest=latest,
+            start_time=random_minute_between(earliest, latest),
+            interaction=interaction
+        )
+    )
+
+client.run(credentials.bot_token)
