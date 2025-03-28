a0ac8562cee8d008d996522faa4b893101dc07f7
ekrohne <krohneew@gmail.com>
2025-03-27 23:20:50 -0700

    The stretching routine

--- stretching_routine.py
+++ stretching_routine.py
@@ -17,39 +17,80 @@ intents = discord.Intents.default()
 # intents.members = True
 client = MyClient(intents=intents)
 
-@client.event
-async def on_ready():
-    drk = await client.fetch_user(credentials.peeps['drk'])
+# @client.event
+# async def on_ready():
+#     drk = await client.fetch_user(credentials.peeps['drk'])
+#
+#     await drk.send("stretching routine")
+#
+#     for i in range(3):
+#         await asyncio.sleep(5)
+#         await drk.send("hands up")
+#         await asyncio.sleep(20)
+#         await drk.send("hands down")
+#
+#     for i in range(3):
+#         await asyncio.sleep(5)
+#         await drk.send("left hand up")
+#         await asyncio.sleep(20)
+#         await drk.send("left hand down")
+#         await asyncio.sleep(5)
+#         await drk.send("right hand up")
+#         await asyncio.sleep(20)
+#         await drk.send("right hand down")
+#
+#     await drk.send("on the floor")
+#     for i in range(3):
+#         await asyncio.sleep(5)
+#         await drk.send("arch")
+#         await asyncio.sleep(20)
+#         await drk.send("relax")
+#
+#     await asyncio.sleep(5)
+#     await drk.send("roll; knees on the footrest")
+#     await asyncio.sleep(400)
+#
+#     await drk.send("done")
 
-    await drk.send("stretching routine")
+#client.run(credentials.bot_token)
 
-    for i in range(3):
+async def main():
+    async with client:
+        client_task = asyncio.create_task(client.start(credentials.bot_token))
+        await client.wait_until_ready()
+
+        drk = await client.fetch_user(credentials.peeps['drk'])
+
+        await drk.send("stretching routine")
+
+        for i in range(3):
+            await asyncio.sleep(5)
+            await drk.send("hands up")
+            await asyncio.sleep(20)
+            await drk.send("hands down")
+
+        for i in range(3):
+            await asyncio.sleep(5)
+            await drk.send("left hand up")
+            await asyncio.sleep(20)
+            await drk.send("left hand down")
+            await asyncio.sleep(5)
+            await drk.send("right hand up")
+            await asyncio.sleep(20)
+            await drk.send("right hand down")
+
+        await drk.send("on the floor")
+        for i in range(3):
+            await asyncio.sleep(5)
+            await drk.send("arch")
+            await asyncio.sleep(20)
+            await drk.send("relax")
+
         await asyncio.sleep(5)
-        await drk.send("hands up")
-        await asyncio.sleep(20)
-        await drk.send("hands down")
+        await drk.send("roll; knees on the footrest")
+        await asyncio.sleep(400)
 
-    for i in range(3):
-        await asyncio.sleep(5)
-        await drk.send("left hand up")
-        await asyncio.sleep(20)
-        await drk.send("left hand down")
-        await asyncio.sleep(5)
-        await drk.send("right hand up")
-        await asyncio.sleep(20)
-        await drk.send("right hand down")
+        await drk.send("done")
 
-    await drk.send("on the floor")
-    for i in range(3):
-        await asyncio.sleep(5)
-        await drk.send("arch")
-        await asyncio.sleep(20)
-        await drk.send("relax")
-
-    await asyncio.sleep(5)
-    await drk.send("roll; knees on the footrest")
-    await asyncio.sleep(400)
-
-    await drk.send("done")
-
-client.run(credentials.bot_token)
+if __name__ == "__main__":
+    asyncio.run(main())
